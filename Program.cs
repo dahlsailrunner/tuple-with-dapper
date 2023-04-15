06142cfb3de6a95f92957272c20229430ed160b5
Erik Dahl <erik.dahl@realpage.com>
2023-04-15 16:13:06 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using Dapper;
+using Microsoft.Data.Sqlite;
+
+Console.WriteLine("Hello, World!");
+
+
+using var sqliteConn = new SqliteConnection("Data Source=tuple-sample.db");
+
+var ids = sqliteConn.Query<int>("SELECT id FROM customer");
+foreach (var id in ids)
+{
+    Console.WriteLine($"Found id {id} with Query<int>");
+}
+
+var customers = sqliteConn.Query<(int Id, string Name, string FavoriteColor)>
+            (@"SELECT id, 
+                  customer_name, 
+                  fav_color, 
+                  create_date
+             FROM customer").ToList();
+
+var firstCustomerName = customers.First().Name;
+Console.WriteLine($"First customer name is {firstCustomerName} with Query<(int, string, string)>");
+
+// the tuple can be deconstructed into its individual parts
+foreach (var (id, name, color) in customers)
+{
+    Console.WriteLine($"Found customer {id} ({name}) [{color}] with Query<(int, string, string)>");
+}
+
+// or the tuple can be used as a whole
+// this results in the same output as the foreach loop above
+foreach (var tuple in customers)
+{
+    Console.WriteLine($"Found customer {tuple.Id} ({tuple.Name}) [{tuple.FavoriteColor}] with Query<(int, string, string)>");
+}
